f72970235e5f190bc36a56098b153a7837f4f71b
Platypus2025 <authorpaper1@gmail.com>
2025-11-17 14:04:24 +0100

    automated script

--- llvm-platypus/README.md
+++ llvm-platypus/README.md
@@ -2,4 +2,13 @@
 
 This folder contains the files we modified. We keep the path names the same as in the official llvm repo.
 
-* Soon a script that makes the setup automated is going to be added.
+### Build
+To build the modified compiler and linker you have to use the provided `compile.sh` script.
+
+The script is going to clone the llvm repo, checkout to `20.1.0` version, change only the modified files
+and then build everything inside a `build` directory in the downloaded llvm repo.
+For this to work you have to provide two arguments. The first is the path to the current repo (e.g. `/path/to/Platypus`). The second is the path inside which you want to clone the llvm repo and build the toolchain. An example follows:
+
+```
+➜ ./compiler.sh /path/to/Platypus /path/to/modified/LLVM/repo
+```
---
+++ llvm-platypus/compiler.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 PATH_TO_PLATYPUS TARGET_DIR"
+    exit 1
+fi
+
+
+PATH_TO_PLATYPUS="$1"
+TARGET_DIR="$2"
+
+
+cd "$TARGET_DIR" || { echo "Failed to cd into $CLONE_DIR"; exit 1; }
+
+
+git clone https://github.com/llvm/llvm-project.git
+
+cd llvm-project/
+
+git checkout llvmorg-20.1.0
+
+
+LLD_FILES="${PATH_TO_PLATYPUS}/llvm-platypus/lld/ELF"
+PATH_TO_LLVM="."
+
+echo "PATH_TO_PLATYPUS: $PATH_TO_PLATYPUS"
+echo "LLD_FILES: $LLD_FILES"
+echo "PATH_TO_LLVM: $PATH_TO_LLVM"
+
+
+
+cp "${LLD_FILES}/Driver.cpp" "${PATH_TO_LLVM}/lld/ELF/Driver.cpp"
+cp "${LLD_FILES}/Config.h" "${PATH_TO_LLVM}/lld/ELF/Config.h"
+cp "${LLD_FILES}/DriverUtils.cpp" "${PATH_TO_LLVM}/lld/ELF/DriverUtils.cpp"
+cp "${LLD_FILES}/InputFiles.cpp" "${PATH_TO_LLVM}/lld/ELF/InputFiles.cpp"
+cp "${LLD_FILES}/InputSection.cpp" "${PATH_TO_LLVM}/lld/ELF/InputSection.cpp"
+cp "${LLD_FILES}/LinkerScript.cpp" "${PATH_TO_LLVM}/lld/ELF/LinkerScript.cpp "
+cp "${LLD_FILES}/MarkLive.cpp" "${PATH_TO_LLVM}/lld/ELF/MarkLive.cpp"
+cp "${LLD_FILES}/OutputSections.cpp" "${PATH_TO_LLVM}/lld/ELF/OutputSections.cpp"
+cp "${LLD_FILES}/Relocations.cpp" "${PATH_TO_LLVM}/lld/ELF/Relocations.cpp"
+cp "${LLD_FILES}/Symbols.cpp" "${PATH_TO_LLVM}/lld/ELF/Symbols.cpp"
+cp "${LLD_FILES}/Symbols.h" "${PATH_TO_LLVM}/lld/ELF/Symbols.h"
+cp "${LLD_FILES}/SymbolTable.cpp" "${PATH_TO_LLVM}/lld/ELF/SymbolTable.cpp"
+cp "${LLD_FILES}/SymbolTable.h" "${PATH_TO_LLVM}/lld/ELF/SymbolTable.h"
+cp "${LLD_FILES}/SyntheticSections.cpp" "${PATH_TO_LLVM}/lld/ELF/SyntheticSections.cpp"
+cp "${LLD_FILES}/SyntheticSections.h" "${PATH_TO_LLVM}/lld/ELF/SyntheticSections.h"
+cp "${LLD_FILES}/Target.h" "${PATH_TO_LLVM}/lld/ELF/Target.h"
+cp "${LLD_FILES}/Writer.cpp" "${PATH_TO_LLVM}/lld/ELF/Writer.cpp"
+
+cp "${LLD_FILES}/Arch/X86_64.cpp" "${PATH_TO_LLVM}/lld/ELF/Arch/X86_64.cpp"
+
+
+cp "${LLD_FILES}/../../llvm/include/llvm/BinaryFormat/ELFRelocs/x86_64.def" "${PATH_TO_LLVM}/llvm/include/llvm/BinaryFormat/ELFRelocs/x86_64.def"
+
+cp "${LLD_FILES}/../../llvm/lib/Target/X86/CMakeLists.txt" "${PATH_TO_LLVM}/llvm/lib/Target/X86/CMakeLists.txt"
+cp "${LLD_FILES}/../../llvm/lib/Target/X86/X86CodeGenPassBuilder.cpp" "${PATH_TO_LLVM}/llvm/lib/Target/X86/X86CodeGenPassBuilder.cpp"
+cp "${LLD_FILES}/../../llvm/lib/Target/X86/X86InstrumentJmpPass.cpp" "${PATH_TO_LLVM}/llvm/lib/Target/X86/X86InstrumentJmpPass.cpp"
+cp "${LLD_FILES}/../../llvm/lib/Target/X86/X86InstrumentJmpPass.h" "${PATH_TO_LLVM}/llvm/lib/Target/X86/X86InstrumentJmpPass.h"
+cp "${LLD_FILES}/../../llvm/lib/Target/X86/X86TargetMachine.cpp" "${PATH_TO_LLVM}/llvm/lib/Target/X86/X86TargetMachine.cpp"
+
+
+mkdir -p build
+cd build
+
+cmake -G Ninja ../llvm \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_ENABLE_PROJECTS="clang;lld;llvm" \
+    -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DCMAKE_INSTALL_PREFIX="$PWD/../install"
+
+ninja -j8
